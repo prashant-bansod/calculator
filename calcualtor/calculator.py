1ac9b3e01387c59e36c7092382c671a5ba090f7e
prashant <prashant.b@vaak.co>
2019-07-29 19:19:19 +0900

    first commit

---
+++ calcualtor/calculator.py
@@ -0,0 +1,12 @@
+""" 
+Cacluator library containing basic math operations
+"""
+
+
+def add(first_term, seconda_term):
+    return first_term + seconda_term
+
+
+def subtract(first_term, seconda_term):
+    return first_term - seconda_term
+
